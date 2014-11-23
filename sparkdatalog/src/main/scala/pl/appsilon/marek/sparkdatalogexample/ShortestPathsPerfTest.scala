d048cc037fe520500bde9015ca7c35024be9b548
Marek Rogala <marek@appsilon.pl>
2014-11-23 17:06:18 +0100

    rmat more result

--- sparkdatalog/src/main/scala/pl/appsilon/marek/sparkdatalogexample/ShortestPathsPerfTest.scala
+++ sparkdatalog/src/main/scala/pl/appsilon/marek/sparkdatalogexample/ShortestPathsPerfTest.scala
@@ -68,7 +68,7 @@ object ShortestPathsPerfTest extends PerformanceTest
         (a,b) => math.min(a,b) // Merge Message
       )
 
-    println("sssp" + sssp.vertices.collect().map(_._2).sum)
+    println("sssp" + sssp.vertices.map(v => (v._1, v._2)).reduce({case (a, b) => (a._1 + b._1, a._2 + b._2)}))
   }
 
   override def runDatalog(): Unit = {
